3521f80dbce2d42997b840e4ae5289bafb3a53ed
midhruvjaink <blivefoundations@gmail.com>
2017-01-20 00:35:29 +0530

    download working well and manifest add

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+function wrap(){
+  html2canvas($(".mag-wrap"), {
+      onrendered: function(canvas) {
+          myImage = canvas.toDataURL("image/png");
+          download(myImage);
+      }
+  });
+}
+
+function download(a){
+  $("#downloadLink").html('<a href="'+a+'" download="imaginator.png"><img class="downloadImg" src="https://cdn2.iconfinder.com/data/icons/snipicons/500/download-48.png"/></a>');
+}
+
 $('#fancy-text').keyup( function(){
   var value = $(this).val();
-  $('.headline').text(value);
+  $('.quote').text(value);
+  setTimeout(function () {
+    wrap();
+  }, 200);
 });
 
 $('form').submit(function(e) {
@@ -8,24 +24,16 @@ $('form').submit(function(e) {
     return false;
 });
 
-$(document).ready(function() {
-  //save button
-  function download(a){
-    $("#downloadLink").html('<a href="'+a+'" download="imaginator.png"><i class="material-icons">cloud_download</i></a>');
-  }
-    $('button').on('click', function() {
-        html2canvas($(".mag-wrap"), {
-            onrendered: function(canvas) {
-                var myImage = canvas.toDataURL("image/png");
-                download(myImage);
-            }
-        });
-        //$(".downloadLink").href = myImage;
-        link = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
-        $.getJSON(link, function(json){
-          $(".quote").html(json.quoteText);
-        });
+$(document).ready(function(){
+  $("#random").on('click', function(){
+    link = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+    $.getJSON(link, function(json){
+      $(".quote").html(json.quoteText);
     });
+    setTimeout(function (){
+      wrap();
+    }, 200);
+  });
 });
 
 //twitter share
