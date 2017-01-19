505f215ec8ae7c4f4912921bcb5567d30866a265
midhruvjaink <blivefoundations@gmail.com>
2017-01-20 02:25:47 +0530

    random colors, and color selection

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # imaginator
-Generates Random Quotes :speech_balloon: Images! 
+### Generates Random Quotes :speech_balloon: Images! [midhruvjaink.github.io/imaginator](https://midhruvjaink.github.io/imaginator)
+
+![N|Solid](https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678136-shield-warning-20.png)If the random quotes are not generated, Please allow **Loading Unsafe Scripts.** As the quotes API is served over http.
+
+## APIS and Libraries Used-
+* The images are generated using the [html2canvas](https://html2canvas.hertzen.com/) library.
+* Random quotes are generated and fetched from [forismatic](forismatic.com/en/api/) API.
--- script.js
+++ script.js
@@ -24,15 +24,56 @@ $('form').submit(function(e) {
     return false;
 });
 
+function randomQuote(){
+  link = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+  $.getJSON(link, function(json){
+    $(".quote").html(json.quoteText);
+  });
+}
+function randomScheme(){
+  bg = ['#B71C1C','#F44336','#E91E63','#880E4F','#4A148C','#9C27B0','#673AB7','#311B92','#3F51B5','#1A237E','#fffff','#0D47A1','#03A9F4','#039BE5','#76FF03','#C6FF00','#FBC02D','#FFD600','#FFAB00','#FF6F00','#FF5722'];
+  font = ['#B71C1C','#F44336','#E91E63','#880E4F','#4A148C','#9C27B0','#673AB7','#311B92','#3F51B5','#1A237E','#fffff','#0D47A1','#03A9F4','#039BE5','#76FF03','#C6FF00','#FBC02D','#FFD600','#FFAB00','#FF6F00','#FF5722'];
+  bgI = Math.floor((Math.random()*21)+1);
+  fontI = Math.floor((Math.random()*21)+1);
+  if(fontI === bgI){
+    randomScheme();
+  }
+  bgChange(bg[bgI]);
+  quoteChange(font[fontI]);
+}
+function bgChange(a) {
+  $(".mag").css('background-color',a);
+}
+
+function quoteChange(a){
+  $(".quote").css('color',a);
+}
+
 $(document).ready(function(){
+  //combination
+  randomScheme();
+  randomQuote();
+  setTimeout(function (){
+    wrap();
+  }, 200);
+  $("#bg").change(function() {
+    bgColor = $("#bg").val();
+    bgChange(bgColor);
+  });
+  $("#font").change(function() {
+    fontColor = $("#font").val();
+    quoteChange(fontColor);
+  });
+
   $("#random").on('click', function(){
-    link = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
-    $.getJSON(link, function(json){
-      $(".quote").html(json.quoteText);
-    });
+    randomQuote();
+    randomScheme();
     setTimeout(function (){
       wrap();
     }, 200);
+  });
+  $("#changeCombination").on('click', function() {
+    randomScheme();
   });
 });
 
